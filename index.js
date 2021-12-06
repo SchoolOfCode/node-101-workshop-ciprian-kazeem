f1d4aca0363410a3ea4886a7a8de7e68f48e4e35
21ciprian <2021ciprianoffice@gmail.com>
2021-12-06 16:46:00 +0000

    Bonus *a

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ import chalk from "chalk";
 function describeItem(item) {
   if (item.count === 1) {
     console.log(
-      `I have a ${chalk.cyan(
+      chalk.bold.bgGray(`I have a ${chalk.cyan(
         item.name
       )}. Here is what I like about it: ${chalk.green(item.whatILike)}.`
-    );
+      ));
   } else {
     console.log(
-      `I have ${chalk.yellow(item.count)} ${chalk.cyan(
+      chalk.bold.bgGray(`I have ${chalk.yellow(item.count)} ${chalk.cyan(
         item.name
       )}s. Here is what I like about them: ${chalk.green(item.whatILike)}.`
-    );
+      ));
   }
 }
 
