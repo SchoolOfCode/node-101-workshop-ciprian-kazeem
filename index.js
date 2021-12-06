cfe2eba16448a48059cfacc92dde8f4b744e624c
21ciprian <2021ciprianoffice@gmail.com>
2021-12-06 16:32:37 +0000

    Part 3

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!",
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory",
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)",
-  },
-];
+import collection from './collection.js'
 // console.log(myCollection)
 // 👉 2c. Create a function called `describeItem`, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should `console.log` a message according to how many of the item you have.
 
@@ -41,4 +25,4 @@ function describeItem(item) {
 function describeCollection(array) {
   array.forEach(describeItem);
 }
-describeCollection(myCollection);
+describeCollection(collection);
